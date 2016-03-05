45ccfd3b5f00304c7ce5a79ca3153582522ed759
Derek P Sifford <dereksifford@gmail.com>
2016-03-05 11:31:31 -0500

    Add plugin install method to run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,11 +4,33 @@ if [ ! -f /app/wp-config.php ]; then
     /app/init-wp
 fi
 
-
 if [ ! -f /.mysql_db_created ]; then
     /app/init-db
+    if [[ ! $(wp core is-installed --allow-root) ]]; then
+        wp core install --allow-root \
+            --url=localhost:8080 \
+            --title=$DB_NAME \
+            --admin_user=root \
+            --admin_password=$DB_PASS \
+            --admin_email=admin@${DB_NAME}.com \
+            --skip-email
+    fi
 fi
 
+[ $PLUGINS ] && \
+    while IFS=',' read -ra plugin; do
+        for i in "${plugin[@]}"; do
+            echo Installing plugin: $i
+            wp plugin install $i --activate --allow-root
+        done
+    done <<< "$PLUGINS"
+
+
+[ -d /app/wp-content/plugins/akismet ] && \
+    wp plugin uninstall akismet hello --deactivate --allow-root
+
+[ -d /app/wp-content/themes/twentyfifteen ] && \
+    wp theme delete twentyfifteen twentyfourteen --allow-root
 
 source /etc/apache2/envvars
 exec apache2 -D FOREGROUND
