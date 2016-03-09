907707466fd7682ed0bbc32d352e101a534ab59c
Derek P Sifford <dereksifford@gmail.com>
2016-03-08 21:35:44 -0500

    Refactor everything in favor of wp-cli

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,35 +1,51 @@
 #!/bin/bash
 
+[ "$DB_NAME" ] || DB_NAME='wordpress'
+[ "$DB_PASS" ] || DB_PASS='root'
+
+# Pause until MySQL is available for connections
+printf "=> Waiting for MySQL to initialize... \n"
+while ! mysqladmin ping --host=db --password=$DB_PASS --silent; do
+    sleep 1
+done
+
+# Configure WordPress
+printf "=> Configuring WordPress...\n"
 if [ ! -f /app/wp-config.php ]; then
     /app/init-wp
+else
+    printf "=> wp-config.php exists. SKIPPING...\n\n"
 fi
 
-if [ ! -f /.mysql_db_created ]; then
+# Install Database
+if [ ! "$(wp core is-installed --allow-root >/dev/null 2>&1 && echo $?)" ]; then
     /app/init-db
-    if [[ ! $(wp core is-installed --allow-root) ]]; then
-        wp core install --allow-root \
-            --url=localhost:8080 \
-            --title="$DB_NAME" \
-            --admin_user=root \
-            --admin_password="$DB_PASS" \
-            --admin_email=admin@"${DB_NAME}".com \
-            --skip-email
-    fi
+else
+    printf "=> Database '%s' exists. SKIPPING...\n\n" "$DB_NAME"
 fi
 
+# Adjust Filesystem Permissions
+printf "=> Adjusting Filesystem Permissions... "
+groupadd -f docker && usermod -aG docker www-data
+find /app -type d -exec chmod 755 {} \;
+find /app -type f -exec chmod 644 {} \;
+chmod -R 775 /app/wp-content/uploads \
+    && chown -R :docker /app/wp-content/uploads
+printf "Done!\n\n"
+
+# Install Plugins
 [ "$PLUGINS" ] && \
     while IFS=',' read -ra plugin; do
-        for i in "${plugin[@]}"; do
-            wp plugin is-installed "$i" --allow-root
+        for i in "${!plugin[@]}"; do
+            wp plugin is-installed "${plugin[$i]}" --allow-root
             if [ $? -eq 0 ]; then
-                echo "=> $i is already installed -- SKIPPING..."
+                printf "=> ($((i+1))/${#plugin[@]}) Plugin '%s' already installed. SKIPPING...\n" "${plugin[$i]}"
             else
-                echo "=> Installing plugin: $i"
-                wp plugin install "$i" --allow-root
+                printf "=> ($((i+1))/${#plugin[@]}) Installing plugin: %s\n" "${plugin[$i]}"
+                wp plugin install "${plugin[$i]}" --allow-root
             fi
         done
     done <<< "$PLUGINS"
-
 
 [ -d /app/wp-content/plugins/akismet ] && \
     wp plugin uninstall akismet hello --deactivate --allow-root
